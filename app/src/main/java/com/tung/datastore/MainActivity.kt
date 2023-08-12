4c5fa87f3355cd04b722fc80d19b46321b619902
viettungvuong <viettungvuong@gmail.com>
2023-08-12 21:03:12 +0700

    Xuất ra được lazyColumn

--- app/src/main/java/com/tung/datastore/MainActivity.kt
+++ app/src/main/java/com/tung/datastore/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.tung.datastore
 
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.EditText
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
@@ -26,15 +31,20 @@ import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import androidx.compose.ui.unit.dp
+import androidx.datastore.dataStore
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.singleOrNull
 
+val Context.stringListDataStore: DataStore<StringList> by dataStore(
+    fileName = "StringList.proto",
+    serializer = StringListSerializer
+)
 val Context.dataStorePrefs: DataStore<Preferences> by preferencesDataStore(name = "settings")
 val nameKey=stringPreferencesKey("name") //key chứa tên
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        val prefs = this.dataStorePrefs
 
         setContent {
             Composable(this)
@@ -50,20 +60,43 @@ suspend fun saveNameToPrefs(newName: String, context: Context){
     }
 }
 
+//lấy list
+suspend fun DataStore<StringList>.getStringList(): List<String> {
+    return try {
+        this.data.map { stringList ->
+            stringList.nameList // lấy list to proto
+        }.singleOrNull() ?: emptyList()
+    } catch (e: Exception) {
+        emptyList()
+    }
+}
+
+//cập nhật list
+suspend fun DataStore<StringList>.update(string: String) {
+    this.updateData { stringList ->
+        stringList.toBuilder().addName(string).build()
+    }
+}
+
 @Composable
 fun Composable(context: Context) {
     var name  by remember { mutableStateOf("") }
     //truyền tên sẽ hiện trong TextView
+
     var inputText by remember { mutableStateOf(name) }
+
+    //list
+    var listState = remember { mutableStateListOf<String>() }
 
     LaunchedEffect(true){
         name = context.dataStorePrefs.data.first()[nameKey]?:""
+        listState.addAll(context.stringListDataStore.getStringList())
     }
 
 
     MaterialTheme{
         Column(
-            modifier = Modifier.fillMaxSize(),
+            modifier = Modifier.fillMaxWidth(),
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ){
@@ -73,34 +106,56 @@ fun Composable(context: Context) {
             TextField(value = inputText, onValueChange = {inputText = it}, label = { Text("Name") })
 
             Box(modifier = Modifier.padding(vertical = 50.dp)) {
-                Button(onClick = {
-                    name = inputText
+                Row {
+                    Button(onClick = {
+                        name = inputText
 
-                    //chạy trong coroutine kotlin
-                    runBlocking {
-                        launch {
-                            saveNameToPrefs(inputText, context)
+                        //chạy trong coroutine kotlin
+                        runBlocking {
+                            launch {
+                                saveNameToPrefs(inputText, context)
+                            }
                         }
+
+                        //hiện toast thông báo
+                        Toast.makeText(context, "Saved $inputText successfully", Toast.LENGTH_SHORT)
+                            .show()
+
+                    }) {
+                        Text("Change name")
                     }
 
-                    //hiện toast thông báo
-                    Toast.makeText(context, "Saved $inputText successfully", Toast.LENGTH_SHORT)
-                        .show()
+                    Button(onClick = {
+                        listState+=inputText //thêm phần tử mới
 
-                }) {
-                    Text("Change name")
+                        Log.d("list elements",listState[0])
+
+                        runBlocking {
+                            launch {
+                                context.stringListDataStore.update(inputText)
+                            }
+                        }
+                        //cập nhật vào dataStore
+
+                        //hiện toast thông báo
+                        Toast.makeText(context, "Added $inputText successfully", Toast.LENGTH_SHORT)
+                            .show()
+
+                    }) {
+                        Text("Add name")
+                    }
                 }
+
             }
 
-            Box(modifier = Modifier.padding(vertical = 50.dp)) {
-                Button(onClick = {
-
-                }) {
-                    Text("Item List")
+            LazyColumn{
+                itemsIndexed(listState){
+                        index, string -> Text("$index. $string")
                 }
             }
-
         }
+
+
     }
 }
 
